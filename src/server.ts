00a93c4b3a68f3732040876982234656bf03819d
sheikh-faedul <sheikhfaedul@gmail.com>
2025-12-27 17:30:38 +0600

    vercel deploy

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import app from "./app"
 import config from "./config";
 
 const port = config.port;
-
+app.get('/', (req, res) => {
+  res.send('server is running')
+})
 app.listen(5000, () => {
     console.log(`server is running ${port}`)
 })
